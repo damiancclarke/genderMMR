7fe377c7f5d57f3503f95cfa872603654735ad56
damiancclarke <damian.c.clarke@gmail.com>
2015-06-26 16:51:47 -0300

    Trellis plots running for all countries

--- Source/mmrRegionTrends.R
+++ Source/mmrRegionTrends.R
@@ -16,19 +16,49 @@ names(mmr) <- c("regCode","country","regName","years","MMR","MMRate","fert")
 
 
 
-subset <- "Zimbabwe"
-postscript(paste(OUT,subset,".eps",sep=""),
+countryPlot <- function(country) {
+    postscript(paste(OUT,country,".eps",sep=""),
                horizontal = FALSE, onefile = FALSE, paper = "special",
                height=7, width=9)
 
-xyplot(MMR ~ years | regName, data=mmr[mmr$country==subset,], xlab="Years",
-       ylab="MMR",main = subset,scales = list(x = list(rot = 55)),
-       panel = function(x, y) {
-           panel.grid(h = -1, v = 2)
-           panel.xyplot(x, y)
-           llines(lowess(x, y))
-       })
-dev.off()
+    print(xyplot(MMR ~ years | regName, data=mmr[mmr$country==country,],
+                 xlab="Years",ylab="MMR",main = country,
+                 scales = list(x = list(rot = 55)),
+                 panel = function(x, y) {
+                     panel.grid(h = -1, v = 2)
+                     panel.xyplot(x, y)
+                     llines(lowess(x, y))
+                 }))
+    dev.off()
+}
 
-
+countryPlot(country="Benin")
+countryPlot(country="Bolivia")
+countryPlot(country="Brazil")
+countryPlot(country="Burkina-Faso")
+countryPlot(country="Burundi")
+countryPlot(country="Cambodia")
+countryPlot(country="Cameroon")
+countryPlot(country="Chad")
+countryPlot(country="Congo-Brazzaville")
+countryPlot(country="Congo-Democratic-Republic")
+countryPlot(country="Cote-d-Ivoire")
+countryPlot(country="Dominican-Republic")
+countryPlot(country="Ethiopia")
+countryPlot(country="Gabon")
+countryPlot(country="Indonesia")
+countryPlot(country="Madagascar")
+countryPlot(country="Mali")
+countryPlot(country="Morocco")
+countryPlot(country="Mozambique")
+countryPlot(country="Namibia")
+countryPlot(country="Niger")
+countryPlot(country="Peru")
+countryPlot(country="Philippines")
+countryPlot(country="Rwanda")
+countryPlot(country="Senegal")
+countryPlot(country="South-Africa")
+countryPlot(country="Tanzania")
+countryPlot(country="Uganda")
+countryPlot(country="Zimbabwe")
 
